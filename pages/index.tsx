939a2c8cffbe0f574afbfe66a54e75d7355ed0c9
Liliy <867655330@qq.com>
2023-08-20 09:11:29 +0800

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,16 @@ export const getStaticProps: GetStaticProps = async () => {
         email: "burk@prisma.io",
       },
     },
+    {
+      id: "2",
+      title: "Prisma is the perfect ORM for Next.js",
+      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
+      published: false,
+      author: {
+        name: "Nikolas Burk",
+        email: "burk@prisma.io",
+      },
+    },
   ]
   return { 
     props: { feed }, 
